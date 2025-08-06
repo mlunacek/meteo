054a18987da17f64bfa362fe6dd139c3b708c614
Monte Lunacek <monte.lunacek@nrel.gov>
2025-08-06 05:31:48 -0600

    updating cognito

--- src/Auth/aws-exports.js
+++ src/Auth/aws-exports.js
@@ -4,7 +4,7 @@ export const awsconfig = {
         Cognito: {
             region: 'us-west-2',
             userPoolId: 'us-west-2_Z93hHZ460',
-            userPoolClientId: '6ibf5vvu3msfemtaiu1td2escr' // ✅ your App Client ID
+            userPoolClientId: '7dmttj2cql529tm2okmjpib7ga' // ✅ your App Client ID
         }
     },
 };
